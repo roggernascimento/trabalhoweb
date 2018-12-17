f39797aaf44ef4ed96c4b4851008a1d7c1148519
kepry <kepridesousa@gmail.com>
2018-12-17 17:53:16 -0200

    update
    1+1

--- app/Http/Controllers/Autor.php
+++ app/Http/Controllers/Autor.php
@@ -7,8 +7,19 @@ use Illuminate\Http\Request;
 class Autor extends Controller
 {
     public function listar(){
-        
-        
+            $autor = [
+        'nome' => 'Stephen king'
+    ];
+    
+    $lista_autores = [
+        ['nome' => 'Clarice Lispector'],
+        ['nome' => 'Patativa do Assaré'],
+        ['nome' => 'Friedrich Nietzsche'],
+        ['nome' => ' Jean-Paul Sartre']
+    ];
+    
+    return view('autor', ['autor' => $autor, 'lista_autores' => $lista_autores]);
     }
+    
     
 }
